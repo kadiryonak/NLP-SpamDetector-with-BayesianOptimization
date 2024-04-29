0c7b60b73d7aae1a904c2265477ef2c3484b591d
Kadir Yönak <128465120+kadiryonak@users.noreply.github.com>
2024-04-29 21:37:37 +0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main.py
 from data_preprocessing import DataPreprocessor
+from feature_extraction import FeatureExtractor
 
 def main():
     
@@ -7,6 +8,8 @@ def main():
     preprocessor = DataPreprocessor(file_path)    
     df_processed = preprocessor.load_and_preprocess_data()    
     print(df_processed.head())
-    
+    feature_extractor = FeatureExtractor(df_processed)
+    X, y = feature_extractor.extract_features()
+
 if __name__ == "__main__":
     main()
